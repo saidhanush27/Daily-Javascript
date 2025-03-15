27119b250ffb5dc990f89a5ac8421aa128dea9ef
Sai Dhanush VR <sai2003vr@gmail.com>
2025-03-15 11:55:30 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,94 @@
+//assigning contents correctly
+var a;
+var b = 7
+console.log(a);
+
+var a =6;
+console.log(a);
+
+//initalizing variabes with operators
+
+var c =5;
+var  d=6;
+var e = "i am ";
+
+c = c + 1;
+d = d + 4;
+e = e + "a string";
+console.log(c);
+console.log(d);
+console.log(e);
+
+//camelcase
+var camelCase;
+var fullName;
+
+camelCase = "hello jello!";
+fullName= "sai dhanush";
+
+console.log(camelCase);
+console.log(fullName);
+
+//operators +-*/
+var sum = 10 + 10 ;
+console.log(sum);
+
+//increment and decrement
+var myvar =11;
+myvar ++;
+console.log(myvar);
+var myvar2= 10 ;
+myvar2 --;
+console.log(myvar2);
+
+//remainder
+var rem;
+rem = 11 % 3;
+console.log(rem);
+
+
+//operating +-*/ values to variables and assinging values
+ f=8;
+ h= 9 ;
+
+ f += 1;
+ h += 2;
+ console.log(f);
+ console.log(h);
+
+ //strings
+ var firstName= "sai";
+ var lastName= " dhanush";
+ console.log(firstName +  lastName);
+
+ //escape characters
+ var myStr = "I am \"Double qouted\" string";
+ console.log(myStr)
+//newlines
+ var mystr2 = "firstline\n\t\\secondline\nthirdline";
+ console.log(mystr2)
+
+ //concsat strings
+ var string = " i am "+ "legend";
+ console.log(string)
+
+
+
+ //plus equals concatination
+ var str3 = "i come first";
+ str3 +=  " i come second";
+ console.log(str3)
+
+
+ //string with variables
+ var news="holiday";
+ var news2= "we have " + news + " today";
+ console.log(news2);
+
+ //append variables with adjectives
+ var adjective ="handsome!";
+ var me = "sai dhanush is "
+me += adjective;
+console.log(me);
+
+ 
